3e3d6f29a2eda7d0287816025713e08096196ddc
dg1223 <alavi1223@hotmail.com>
2023-11-01 18:52:28 -0400

    feature: check if matrix is secondary diagonal

---
+++ semester1/introduction-to-c/matrices/matrix6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+    int row, col;
+    // take input
+    scanf("%d %d", &row, &col);
+    int a[row][col];
+    for (int i=0; i<row; i++){
+        for (int j=0; j<col; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    // check if it's a diagonal matrix
+    int flag = 1;
+    if (row != col){
+        flag = 0;
+    }
+    for (int i=0; i<row; i++){
+        for (int j=0; j<col; j++){
+            // check if primary diagonal
+            if (i + j == row - 1){
+                continue;
+            }
+            if (a[i][j] != 0){
+                flag = 0;
+            }
+        }
+    }
+    if (flag == 1){
+        printf("Secondary diagonal");
+    }
+    else {
+        printf("Not seondary diagonal");
+    }
+    return 0;
+}
