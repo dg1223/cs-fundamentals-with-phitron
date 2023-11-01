5be89dd25101e7173bab3009023e396849b8664b
dg1223 <alavi1223@hotmail.com>
2023-11-01 19:05:16 -0400

    feature: scalar matrix

---
+++ semester1/introduction-to-c/matrices/matrix8.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int row, col;
+    // take input
+    scanf("%d %d", &row, &col);
+    int a[row][col];
+    for (int i=0; i<row; i++){
+        for (int j=0; j<col; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    // check if it's a diagonal matrix
+    int flag = 1;
+    // check if square matrix
+    if (row != col){
+        flag = 0;
+    }
+    for (int i=0; i<row; i++){
+        for (int j=0; j<col; j++){
+            // check if primary diagonal
+            if (i == j){
+                if (a[i][j] != a[0][0]){
+                    flag = 0;
+                }
+                // we should not check the next condition
+                // since values on the diagonal are not 
+                // going to be zero
+                continue;
+            }
+            if (a[i][j] != 0){
+                flag = 0;
+            }            
+        }
+    }
+    if (flag == 1){
+        printf("Scalar matrix");
+    }
+    else {
+        printf("Not scalar matrix");
+    }
+    return 0;
+}
