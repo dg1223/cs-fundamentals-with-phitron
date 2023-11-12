8901b2d641d3258a5834820a004db575977c84e6
dg1223 <alavi1223@hotmail.com>
2023-11-12 00:33:41 -0500

    feature: bits stdc++ header file

---
+++ semester1/introduction-to-c++/bits_header_file.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+//#include <string>
+//#include <iomanip>
+//#include <utility>
+//#include <algorithm>
+
+using namespace std;
+
+int main(){
+    cout << "hello" << endl;
+    return 0;
+}
