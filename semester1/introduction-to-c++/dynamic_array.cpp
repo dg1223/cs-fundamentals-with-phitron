871534f8732a16a0f60b948d1dc89547890d271b
dg1223 <alavi1223@hotmail.com>
2023-11-13 02:28:12 -0500

    feature: return dynamic array

---
+++ semester1/introduction-to-c++/dynamic_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *a = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " " << endl;
+    }
+    return 0;
+}
