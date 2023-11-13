3451523875e34e8c920580012ec5e402f4dc6114
dg1223 <alavi1223@hotmail.com>
2023-11-13 02:43:58 -0500

    feature: increase size of dynamic array

---
+++ semester1/introduction-to-c++/increase_size_of_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int *a = new int[3];
+    int *b = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> a[i];
+        b[i] = a[i];
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        cout << b[i] << " " << endl;
+    }
+    delete[] a;
+
+    a = new int[5];
+    for (int i = 0; i < 3; i++)
+    {
+        a[i] = b[i];
+    }
+    delete[] b;
+    a[3] = 4;
+    a[4] = 5;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " " << endl;
+    }
+    return 0;
+}
