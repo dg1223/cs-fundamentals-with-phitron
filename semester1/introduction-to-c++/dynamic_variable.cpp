8cbc56a08746f3df98133b194f183bca73bb7e99
dg1223 <alavi1223@hotmail.com>
2023-11-13 02:10:52 -0500

    feature: dynamic variable

---
+++ semester1/introduction-to-c++/dynamic_variable.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int **fun2()
+{
+    int *x = new int;
+    *x = 100;
+    return &x;
+}
+
+int *fun()
+{
+    int *a = new int;
+    *a = 10;
+    return a;
+}
+
+int main()
+{
+    // int x = 10;
+    // int *a = new int;
+    //*a = 10;
+
+    int *a = fun();
+    cout << a << " " << *a << endl;
+
+    // this should throw an error
+    // x's address was kept in static memory and cleared
+    // after function was returned
+    int **b = fun2();
+    cout << b << " " << *b << " " << **b << endl;
+    return 0;
+}
