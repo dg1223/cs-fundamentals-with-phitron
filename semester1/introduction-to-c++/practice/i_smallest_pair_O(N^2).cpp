c545ba784ff9fb32e7e585e9580848e569302bcd
dg1223 <alavi1223@hotmail.com>
2023-11-16 15:43:34 -0500

    feature: find smallest possible pair, quadratic time

---
+++ semester1/introduction-to-c++/practice/i_smallest_pair_O(N^2).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(){
+    int testcases;
+    cin >> testcases;
+    for (int x=0; x<testcases; x++){
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i=0; i<n; i++){
+            cin >> a[i];
+        }
+
+        int smallest = INT_MAX;
+        for (int i=0; i<n; i++){
+            for (int j=i+1; j<n; j++){
+                int result = a[i] + a[j] + j - i;
+                if (result < smallest){
+                    smallest = result;
+                }
+            }
+        }
+
+        cout << smallest << endl;
+    }
+
+    return 0;
+}
