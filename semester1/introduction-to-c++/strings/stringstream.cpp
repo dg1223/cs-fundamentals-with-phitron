4c1db262af1078daeb7e0763c01a8d91e0e9e9eb
dg1223 <alavi1223@hotmail.com>
2023-11-18 23:02:10 -0500

    feature: string stream

---
+++ semester1/introduction-to-c++/strings/stringstream.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+
+    // ss >> word;
+    // cout << word << endl;
+    // ss >> word;
+    // cout << word << endl;
+    // ss >> word;
+    // cout << word << endl;
+    
+    int count = 0;
+    while (ss >> word){
+        cout << word << endl;
+        count++;
+    }
+
+    cout << count << endl;
+    return 0;
+}
