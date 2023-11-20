4244ab16a5d81f1a8e1b7751b20ebe38c3964d2d
dg1223 <alavi1223@hotmail.com>
2023-11-20 02:46:09 -0500

    feature: string constructor

---
+++ semester1/introduction-to-c++/strings/string_constructor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // string s = "hello world";
+    // constructor
+    // string s("hello world");
+    // string s("hello world", 4);
+    // string a = "hello world";
+    // string s(a, 4);
+    string s(5, 'a');
+    cout << s << endl;
+
+    return 0;
+}
