607dfb1806fb13b99fe97237448877244a192a74
dg1223 <alavi1223@hotmail.com>
2023-11-28 01:59:41 -0500

    chores: added time complexities of loops

--- semester2/data_structures/O_N_log_N.cpp
+++ semester2/data_structures/O_N_log_N.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n; i++){ // O(N)
         int x = i;
-        while (x > 0){
+        while (x > 0){ // O(log(N))
                 int digit = x % 10;
                 cout << digit << " ";
                 x /= 10;
