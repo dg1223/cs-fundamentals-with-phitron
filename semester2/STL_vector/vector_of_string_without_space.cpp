a74af4b294576718a7ba4c54d2c5b8690bebcc4e
dg1223 <alavi1223@hotmail.com>
2023-11-29 09:19:48 -0500

    feature: vector of string input with spaces

---
+++ semester2/STL_vector/vector_of_string_without_space.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    // consider everything including spaces as
+    // a single string until it hits newline
+    cin >> n;
+
+    vector<string> v;
+
+    for (int i=0; i<n; i++){
+        string s;
+        cin >> s;
+        v.push_back(s);
+    }
+
+    // // method 1 to print vector of string
+    // for (int i=0; i<v.size(); i++){
+    //     cout << v[i] << endl;
+    // }
+
+    // method 2
+    for(string val:v) {
+        cout << val <<endl;
+    }
+    
+
+    return 0;
+}
