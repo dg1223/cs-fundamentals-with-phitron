72f7e38be3084a4e0145edc9152894f8bc5ed2af
dg1223 <alavi1223@hotmail.com>
2023-12-01 06:19:42 -0500

    debug: binary search codeforces

--- semester2/data_structures/binary_search/z_binary_search.cpp
+++ semester2/data_structures/binary_search/z_binary_search.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
-    int n, x;
-    cin >> n;
+    int n, q;
+    cin >> n >> q;
     int a[n];
     for (int i=0; i<n; i++){
         cin >> a[i];
     }
 
-    cin >> x;
+    sort(a, a+n);
 
-    int l = 0, r = n-1;
-    bool flag = false;
-    while (l <= r){
-        int mid = (l + r) / 2;
-        if (a[mid] == x){
-            flag = true;
-            break;
+    while (q--){
+        int x;
+        cin >> x;
+
+        // binary search
+        int l = 0, r = n-1;
+        bool flag = false;
+        while (l <= r){
+            int mid = (l + r) / 2;
+            if (a[mid] == x){
+                flag = true;
+                break;
+            }
+            if (x > a[mid]){
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
         }
-        if (x > a[mid]){
-            l = mid + 1;
+
+        if (flag == true){
+            cout << "found" << endl;
         }
         else {
-            r = mid - 1;
+            cout << "not found" << endl;
         }
-    }
 
-    if (flag){
-        cout << "YES" << endl;
     }
-    else {
-        cout << "NO" << endl;
-    }
-
     return 0;
 }
