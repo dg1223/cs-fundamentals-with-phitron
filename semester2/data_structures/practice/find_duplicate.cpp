dab2d3c387683aa3ce1267adc9e28f75630b4b07
dg1223 <alavi1223@hotmail.com>
2023-12-07 19:48:41 -0500

    feature: find if array has duplicate

---
+++ semester2/data_structures/practice/find_duplicate.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> hashmap;
+
+    // a
+    for (int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    bool HAS_DUPLICATE = false;
+
+    for (int i=0; i<n; i++){
+        auto it = find(hashmap.begin(), hashmap.end(), a[i]);
+        if (it != hashmap.end()){
+            HAS_DUPLICATE = true;
+        }
+        else {
+            hashmap.push_back(a[i]);
+        }
+    }
+
+    // for (int i=0; i<hashmap.size(); i++){
+    //     cout << hashmap[i] << " " << endl;
+    // }
+
+    if (HAS_DUPLICATE){
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+
+
+
+    return 0;
+}
