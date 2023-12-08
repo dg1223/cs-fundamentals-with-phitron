b7abcadbb1d3232bcd464f860d104e1c093c6df5
dg1223 <alavi1223@hotmail.com>
2023-12-07 23:05:21 -0500

    feature: pattern

--- semester2/data_structures/practice/pattern_printing.cpp
+++ semester2/data_structures/practice/pattern_printing.cpp
@@ -9,26 +9,18 @@ int main() {
     int n;
     cin >> n;
 
-    // Check if N is odd
-    if (n % 2 == 0) {
-        cerr << "Input must be an odd integer." << endl;
-        return 1;
-    }
-
-    // Initialize the pattern
     vector<string> pattern(n, string(n, ' '));
 
-    // Fill the pattern
     for (int i = 0; i < n; ++i) {
         pattern[i][i] = '\\';
         pattern[i][n - i - 1] = '/';
     }
 
-    // Place the X in the middle
+    // X
     int middle = n / 2;
     pattern[middle][middle] = 'X';
 
-    // Output the pattern
+
     for (int i = 0; i < n; ++i) {
         cout << pattern[i] << endl;
     }
