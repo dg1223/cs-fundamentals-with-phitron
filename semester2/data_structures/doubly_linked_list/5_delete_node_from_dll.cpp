883706e594fdb4f866369c37fd5b4420c867453e
dg1223 <alavi1223@hotmail.com>
2023-12-10 03:54:42 -0500

    feature: delete nodes from dll

--- semester2/data_structures/doubly_linked_list/5_delete_node_from_dll.cpp
+++ semester2/data_structures/doubly_linked_list/5_delete_node_from_dll.cpp
@@ -35,46 +35,6 @@ void print_reverse(Node *tail) {
     cout << endl;
 }
 
-// valid for any position except head and tail
-// O(N) time
-void insert_at_position(Node *head, int position, int value) {
-    Node *new_node = new Node(value);
-    Node *current = head;
-    for (int i=1; i<=position-1; i++){
-        current = current->next;
-        }
-    new_node->next = current->next; // 100 -> 30
-    current->next = new_node; // 20 -> 100
-    new_node->next->prev = new_node; // 100 <- 30
-    new_node->prev = current; // 20 <- 100
-}
-
-// O(1) time
-void insert_head(Node *&head, Node *&tail, int value) {
-    Node *new_node = new Node(value);
-    if (head == NULL){
-        head = new_node;
-        tail = new_node;
-        return;
-    }
-    new_node->next = head;
-    head->prev = new_node;
-    head = new_node;
-}
-
-// O(1) time
-void insert_tail(Node *&head, Node *&tail, int value) {
-    Node *new_node = new Node(value);
-    if (tail == NULL) {
-        head = new_node;
-        tail = new_node;
-        return;
-    }
-    tail->next = new_node;
-    new_node->prev = tail;
-    tail = tail->next;
-}
-
 void delete_at_position(Node *head, int position) {
     Node *current = head;
     for (int i=1; i<=position-1; i++){
@@ -112,44 +72,42 @@ int size(Node *head) {
     return count;
 }
 
-
-
 int main(){
-    // Node *head = NULL;
-    // Node *tail = NULL;
-    Node *head = new Node(10);
-    Node *a = new Node(20);
-    Node *b = new Node(30);
-    Node *c = new Node(40);
-    Node *tail = c;
+    Node *head = NULL;
+    Node *tail = NULL;
+    // Node *head = new Node(10);
+    // Node *a = new Node(20);
+    // Node *b = new Node(30);
+    // Node *c = new Node(40);
+    // Node *tail = c;
 
-    // update connections
-    head->next = a;
-    a->prev = head;
-    a->next = b;
-    b->prev = a;
-    b->next = c;
-    c->prev = b;
+    // // update connections
+    // head->next = a;
+    // a->prev = head;
+    // a->next = b;
+    // b->prev = a;
+    // b->next = c;
+    // c->prev = b;
 
-    int position, value;
-    cin >> position >> value;
+    int position;
+    cin >> position;
     // insert_tail(head, tail, value);
     // delete_at_position(head, position);
     // delete_tail(tail);
-    delete_head(head);
+    // delete_head(head);
 
-    // if (position == 0){
-    //     insert_head(head, tail, value);
-    // }
-    // if (position == size(head)){
-    //     insert_tail(head, tail, value);
-    // }
-    // else if (position > size(head)) {
-    //     cout << "Invalid" << endl;
-    // }
-    // else {        
-    //     insert_at_position(head, position, value);
-    // }
+    if (position == 0){
+        delete_head(head);
+    }
+    else if (position == size(head)-1){
+        delete_tail(tail);
+    }
+    else if (position >= size(head)) {
+        cout << "Invalid" << endl;
+    }
+    else {        
+        delete_at_position(head, position);
+    }
 
 
     print_normal(head);
