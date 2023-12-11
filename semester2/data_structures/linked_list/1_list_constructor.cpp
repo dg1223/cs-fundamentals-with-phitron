0251f51e1e9b013d517c9421b90b9ee83cc76994
dg1223 <alavi1223@hotmail.com>
2023-12-11 08:46:27 -0500

    feature: initialize list

---
+++ semester2/data_structures/linked_list/1_list_constructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    // // regular initialization
+    // list<int> my_list;
+    // list<int> my_list(10, 5);
+    // list<int> my_list2(my_list);
+
+    // // initialize from array
+    // int a[5] = {10, 20 ,30 ,40 ,50};
+    // list<int> my_list(a, a+5);
+
+    // initialize from vector
+    vector<int> v = {100, 200, 300};
+    list<int> my_list(v.begin(), v.end());
+
+
+    // for (auto it=my_list.begin(); it!=my_list.end(); it++){
+    //     cout << *it << endl;
+    // }
+
+    for(int val : my_list) {
+        cout << val << endl;
+    }
+    return 0;
+}
