c4205256cb07de5db7512687a0cf253bce06c9c4
dg1223 <alavi1223@hotmail.com>
2023-12-11 10:08:21 -0500

    feature: detect cycle in linked list

--- semester2/data_structures/linked_list/reverse_doubly_linked_list.cpp
+++ semester2/data_structures/linked_list/reverse_doubly_linked_list.cpp
@@ -6,34 +6,25 @@ class Node {
     public:
         int value;
         Node *next;
-        Node *prev;
 
     Node(int value){
         this->value = value;
         this->next = NULL;
-        this->prev = NULL;
     }
 };
 
-void print_normal(Node *head) {
-    Node *current = head;
-    while (current != NULL){
-        cout << current->value << " ";
-        current = current->next;
+bool detect_cycle(Node *head) {
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL){
+        // start the race first, then check
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow){
+            return true;
+        }
     }
-    cout << endl;
-}
-
-// two pointers
-void reverse(Node *head, Node *tail) {
-    Node *i = head;
-    Node *j = tail;
-    // account for both odd and even number of nodes
-    while (i != j && i->next != j){
-        swap(i->value, j->value);
-        i = i->next;
-        j = j->prev;
-    }
+    return false;
 }
 
 int main(){
@@ -41,21 +32,20 @@ int main(){
     Node *a = new Node(20);
     Node *b = new Node(30);
     Node *c = new Node(40);
-    Node *d = new Node(50);
-    Node *tail = d;
 
     // update connections
     head->next = a;
-    a->prev = head;
     a->next = b;
-    b->prev = a;
     b->next = c;
-    c->prev = b;
-    c->next = d;
-    d->prev = c;
+    c->next = b;
 
-    reverse(head, tail);
-    print_normal(head);
+    bool CYCLE_DETECTED = detect_cycle(head);
+    if (CYCLE_DETECTED){
+        cout << "Cycle detected" << endl;
+    }
+    else {
+        cout << "No cycle detected" << endl;
+    }
 
     return 0;
 }
