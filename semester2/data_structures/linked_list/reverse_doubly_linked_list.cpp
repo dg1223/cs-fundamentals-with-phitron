c9f2a3dcbe4c4fd0e817680d39f62a483f2cd135
dg1223 <alavi1223@hotmail.com>
2023-12-11 23:41:11 -0500

    feature: reverse doubly linked list - edge case

--- semester2/data_structures/linked_list/reverse_doubly_linked_list.cpp
+++ semester2/data_structures/linked_list/reverse_doubly_linked_list.cpp
@@ -6,25 +6,36 @@ class Node {
     public:
         int value;
         Node *next;
+        Node *prev;
 
     Node(int value){
         this->value = value;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
-bool detect_cycle(Node *head) {
-    Node *slow = head;
-    Node *fast = head;
-    while (fast != NULL && fast->next != NULL){
-        // start the race first, then check
-        slow = slow->next;
-        fast = fast->next->next;
-        if (fast == slow){
-            return true;
-        }
+void print_normal(Node *head) {
+    Node *current = head;
+    while (current != NULL){
+        cout << current->value << " ";
+        current = current->next;
     }
-    return false;
+    cout << endl;
+}
+
+// two pointers
+void reverse(Node *head, Node *tail) {
+    Node *i = head;
+    Node *j = tail;
+    // account for both odd and even number of nodes
+    while (i != j && i->next != j){
+        swap(i->value, j->value);
+        i = i->next;
+        j = j->prev;
+    }
+    // edge case where list has only 1 node or even number of nodes
+    swap(i->value, j->value);
 }
 
 int main(){
@@ -32,20 +43,21 @@ int main(){
     Node *a = new Node(20);
     Node *b = new Node(30);
     Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *tail = d;
 
     // update connections
     head->next = a;
+    a->prev = head;
     a->next = b;
+    b->prev = a;
     b->next = c;
-    c->next = b;
+    c->prev = b;
+    c->next = d;
+    d->prev = c;
 
-    bool CYCLE_DETECTED = detect_cycle(head);
-    if (CYCLE_DETECTED){
-        cout << "Cycle detected" << endl;
-    }
-    else {
-        cout << "No cycle detected" << endl;
-    }
+    reverse(head, tail);
+    print_normal(head);
 
     return 0;
 }
