f473afc8089ad4566e816db2d41136e03d08fecc
dg1223 <alavi1223@hotmail.com>
2024-01-05 15:06:37 -0500

    feature: take tree as input using level order

---
+++ semester2/data_structures/binary_tree/input_binary_tree.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *left;
+        Node *right;
+
+    Node(int value){
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_tree(){
+    int value;
+    cin >> value;
+    Node *root;
+    if (value == -1){
+        root == NULL;
+    }
+    else {
+        root = new Node(value);
+    }
+    
+    queue<Node*> q;
+    if (root){
+        q.push(root);
+    }
+
+    while (!q.empty()){
+        Node *parent = q.front();
+        q.pop();
+
+        // do whatever task you need to perform here
+        int l, r;
+        cin >> l >> r;
+        Node *left;
+        Node *right;
+
+        if (l == -1){
+            left = NULL;
+        }
+        else {
+            left = new Node(l);
+        }
+
+        if (r == -1){
+            right = NULL;
+        }
+        else {
+            right = new Node(r);
+        }
+
+        parent->left = left;
+        parent->right = right;
+
+        // push the children if applicable
+        if (parent->left){
+            q.push(parent->left);
+        }
+        if (parent->right){
+            q.push(parent->right);
+        }
+    }
+
+    return root;
+}
+
+// // uncomment the following block if you want to test the tree
+// void level_order_traversal(Node *root){
+//     // account for empty tree
+//     if (root == NULL){
+//         cout << "Tree is empty" << endl;
+//         return;
+//     }
+
+//     queue<Node*> q;
+//     q.push(root);
+//     while (!q.empty()){
+//         Node *f = q.front();
+//         q.pop();
+
+//         // do whatever task you need to perform here
+//         cout << f->value << " ";
+
+//         // keep the children here if applicable
+//         if (f->left){
+//             q.push(f->left);
+//         }
+//         if (f->right){
+//             q.push(f->right);
+//         }
+//     }
+// }
+
+int main(){
+    Node *root = input_tree();
+
+    // // test
+    // level_order_traversal(root);
+
+    return 0;
+}
