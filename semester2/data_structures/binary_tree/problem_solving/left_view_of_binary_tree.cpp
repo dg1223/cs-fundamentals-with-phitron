7a4df20cfb6d6608cbf73c7ef9757e11843b30ec
dg1223 <alavi1223@hotmail.com>
2024-01-05 23:02:22 -0500

    feature: left view of binary tree

---
+++ semester2/data_structures/binary_tree/problem_solving/left_view_of_binary_tree.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *left;
+        Node *right;
+
+    Node(int value){
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_tree(){
+    int value;
+    cin >> value;
+    Node *root;
+    if (value == -1){
+        root == NULL;
+    }
+    else {
+        root = new Node(value);
+    }
+    
+    queue<Node*> q;
+    if (root){
+        q.push(root);
+    }
+
+    while (!q.empty()){
+        Node *parent = q.front();
+        q.pop();
+
+        // do whatever task you need to perform here
+        int l, r;
+        cin >> l >> r;
+        Node *left;
+        Node *right;
+
+        if (l == -1){
+            left = NULL;
+        }
+        else {
+            left = new Node(l);
+        }
+
+        if (r == -1){
+            right = NULL;
+        }
+        else {
+            right = new Node(r);
+        }
+
+        parent->left = left;
+        parent->right = right;
+
+        // push the children if applicable
+        if (parent->left){
+            q.push(parent->left);
+        }
+        if (parent->right){
+            q.push(parent->right);
+        }
+    }
+
+    return root;
+}
+
+vector<int> getLeftView(Node *root){
+	bool freq[3001] = {false};
+    queue<pair<Node*, int>> q;
+
+    // handle empty tree input
+    if (root){
+        q.push({root, 1});
+    }
+    
+    vector<int> v;
+
+    while (!q.empty()){
+        pair<Node*, int> pr = q.front();
+        q.pop();
+        Node *node = pr.first;
+        int level = pr.second;
+
+        if (freq[level] == false){
+            v.push_back(node->value);
+            freq[level] = true;
+        }
+
+        if (node->left){
+            q.push({node->left, level+1});
+        }
+
+        if (node->right){
+            q.push({node->right, level+1});
+        }
+    }
+
+    return v;
+}
+
+int main(){
+    Node *root = input_tree();
+
+    cout << count_leaf(root) << endl;
+
+    return 0;
+}
