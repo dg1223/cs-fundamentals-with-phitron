93baa308f80e40f1e15d87ba030bdc65f12dd294
dg1223 <alavi1223@hotmail.com>
2024-01-06 00:31:47 -0500

    feature: special binary tree

---
+++ semester2/data_structures/binary_tree/problem_solving/special_binary_tree.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *left;
+        Node *right;
+
+    Node(int value){
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* input_tree(){
+    int value;
+    cin >> value;
+    Node *root;
+    if (value == -1){
+        root == NULL;
+    }
+    else {
+        root = new Node(value);
+    }
+    
+    queue<Node*> q;
+    if (root){
+        q.push(root);
+    }
+
+    while (!q.empty()){
+        Node *parent = q.front();
+        q.pop();
+
+        // do whatever task you need to perform here
+        int l, r;
+        cin >> l >> r;
+        Node *left;
+        Node *right;
+
+        if (l == -1){
+            left = NULL;
+        }
+        else {
+            left = new Node(l);
+        }
+
+        if (r == -1){
+            right = NULL;
+        }
+        else {
+            right = new Node(r);
+        }
+
+        parent->left = left;
+        parent->right = right;
+
+        // push the children if applicable
+        if (parent->left){
+            q.push(parent->left);
+        }
+        if (parent->right){
+            q.push(parent->right);
+        }
+    }
+
+    return root;
+}
+
+bool isSpecialBinaryTree(Node* root){
+    if (root->left == NULL && root->right == NULL){
+        return true;
+    }
+    
+    if (root->left == NULL || root->right == NULL){
+        return false;
+    }
+
+    bool l = isSpecialBinaryTree(root->left);
+    bool r = isSpecialBinaryTree(root->right);
+
+    if (l == false || r == false){
+        return false;
+    }
+
+    return true;
+}
+
+int main(){
+    Node *root = input_tree();
+
+    cout << count_leaf(root) << endl;
+
+    return 0;
+}
