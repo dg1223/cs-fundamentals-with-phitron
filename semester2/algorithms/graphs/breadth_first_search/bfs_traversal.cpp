5b7ccdbe830756f143936459ea4febc1d146fd8a
dg1223 <alavi1223@hotmail.com>
2024-01-21 19:05:24 -0500

    feature: bfs graph traversal

---
+++ semester2/algorithms/graphs/breadth_first_search/bfs_traversal.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+// adj list
+vector<int> v[1005];
+// visited list
+bool visited[1005];
+
+void bfs(int source){
+    queue<int> q;
+    q.push(source);
+    visited[source] = true;
+
+    while (!q.empty()){
+        int parent = q.front();
+        q.pop();        
+        cout << parent << endl;
+
+        for (int child : v[parent]){
+            if (!visited[child]){
+                q.push(child);
+                visited[child] = true;
+            }
+        }
+    }
+}
+
+int main(){
+    int n, e;
+    cin >> n >> e;
+
+    while (e--){
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    int source;
+    cin >> source;
+    
+    memset(visited, false, sizeof(visited));
+
+    bfs(source);
+
+    return 0;
+}
+
+/*
+input
+7 12
+0 1
+1 2
+0 4
+1 3
+2 0
+3 4
+1 5
+3 6
+2 5
+5 6
+1 4
+1 6
+0
+*/
