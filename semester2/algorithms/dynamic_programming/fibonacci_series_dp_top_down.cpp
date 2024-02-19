5dc7e5b6e1ca8fcb1fc8eddce334cf8f776049d6
dg1223 <alavi1223@hotmail.com>
2024-02-18 23:56:32 -0500

    feature: fibonacci series dp recursive

---
+++ semester2/algorithms/dynamic_programming/fibonacci_series_dp_top_down.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+#define ll long long int
+
+using namespace std;
+
+const ll N = 1e6+5;
+ll dp[N];
+
+ll fibo(ll n){
+    if (n == 0 || n == 1){
+        return n;
+    }
+
+    // if it's not -1, then it has the answer to the recursive call
+    if (dp[n] != -1){
+        return dp[n];
+    }
+    ll ans = fibo(n-1) + fibo(n-2);
+    dp[n] = ans;
+
+    return ans;
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    memset(dp, -1, sizeof(dp));
+    
+    cout << fibo(n) << endl;
+    return 0;
+}
