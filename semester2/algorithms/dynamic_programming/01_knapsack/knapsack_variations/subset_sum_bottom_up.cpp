98a07c35b0e443ccb38f51c5051e73f25fe451b0
dg1223 <alavi1223@hotmail.com>
2024-02-24 23:48:51 -0500

    feature: subset sum bottom up tabular approach

---
+++ semester2/algorithms/dynamic_programming/01_knapsack/knapsack_variations/subset_sum_bottom_up.cpp
@@ -0,0 +1,92 @@
+/*
+VERY CONFUSING!!!
+NEED BETTER EXPLANATION
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// for memoization
+int dp[1005][1005];
+
+bool subset_sum(int n, int a[], int s){
+    // base case
+    if (n == 0){
+        return s == 0;
+    }
+
+    // use memoization
+    int dp_current = dp[n][s];
+    if (dp_current != -1){
+        return dp_current;
+    }
+    // if current value is within sum
+    bool op2 = subset_sum(n-1, a, s);
+    if (a[n-1] <= s){
+        bool op1 = subset_sum(n-1, a, s-a[n-1]);
+        // bool op2 = subset_sum(n-1, a, s);
+        return dp[n][s] = op1 || op2;
+    }
+    else {
+        return dp[n][s] = op2;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    // sum
+    int s;
+    cin >> s;
+
+    bool dp[n+1][s+1];
+    dp[0][0] = true;
+    for (int i=1; i<=s; i++){
+        dp[0][i] = false;
+    }
+
+    for (int i=1; i<=n; i++){
+        for (int j=0; j<=s; j++){
+            if (a[i-1] <= j){
+                dp[i][j] = dp[i-1][j-a[i-1]] || dp[i-1][j];
+            }
+            else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    for (int i=0; i<=n; i++){
+        for (int j=0; j<=s; j++){
+            if (dp[i][j]){
+                cout << "T ";
+            }
+            else {
+                cout << "F ";
+            }
+        }
+        cout << endl;
+    }
+
+    // if (dp[n][s]){
+    //     cout << "YES" << endl;
+    // }
+    // else {
+    //     cout << "NO" << endl;
+    // }
+
+    return 0;
+}
+
+/*
+4
+1 2 3 6
+7
+*/
